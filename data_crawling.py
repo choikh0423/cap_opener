b548239dde657d5406f74ace477b3b6f8390a6c8
SeungCholCho <seung.chol.cho@gmail.com>
2022-03-29 01:53:50 +0900

    Co-authored-by: Kyu Hwan Choi <choikh0423@users.noreply.github.com> Co-authored-by: Lee-Eui-Sang <Lee-Eui-Sang@users.noreply.github.com>

--- data_crawling.py
+++ data_crawling.py
@@ -4,27 +4,51 @@ import openpyxl
 
 from openpyxl import load_workbook
 
+
 # Finding path for raw data directory
 path = os.getcwd()      
 files = os.listdir(path+"/raw_data")
-#print(files)
+file = "disease.xlsx"
 
-# Remove cell style
-def removeFormatting(ws):
-    for row in ws.iter_rows():
-        for cell in row:
-            cell.style = 'Normal'
+
+print("************************LOADING WORKBOOK\n\n\n")
+
+wb = load_workbook(path+r"/raw_data/"+file)
+ws = wb.active
+
+print("************************BEFORE UNMERGE\n\n\n")
+
+ws.unmerge_cells('A1:R200')
+
+print("************************COMPLETED UNMERGE\n\n\n")
+
+data = pd.read_excel(path+"/raw_data/"+file,'sheet 1')
+data_list = data.values.tolist()
+print(data_list)
+
+clean_data_list = []
+for dl in data_list:
+    clean_data_list.append([x for x in dl if str(x) != 'nan'])
+print(clean_data_list)
+
 
 # Convert excel to list
-for f in files:
-    print(f)
-    data = pd.read_excel(path+"/raw_data/"+f,'sheet 1')
-    data_list = data.values.tolist()
+# for file in files:
+#     print(file)
+#     wb = load_workbook(path+"/raw_data"+file)
 
-    clean_data_list = []
-    for dl in data_list:
-        clean_data_list.append([x for x in dl if str(x) != 'nan'])  
+#     ws = wb.activate
+#     ws.unmerge_cells('A1:R200')
 
-    print(clean_data_list)
+
+
+#     data = pd.read_excel(path+"/raw_data/"+file,'sheet 1')
+#     data_list = data.values.tolist()
+#     print(data_list)
+
+#     clean_data_list = []
+#     for dl in data_list:
+#         clean_data_list.append([x for x in dl if str(x) != 'nan'])  
+    
+    #print(clean_data_list)
     #df = df.append(data)
-
